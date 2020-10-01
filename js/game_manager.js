9b82a5ee14ef8f386039bb6fc91d48e8adbd6f00
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-10-01 11:00:24 -0500

    Create 4194304 variant

--- js/game_manager.js
+++ js/game_manager.js
@@ -170,8 +170,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 4194304 tile
+          if (merged.value === 4194304) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
